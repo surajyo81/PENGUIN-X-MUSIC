4bd82af40a1771bad1ef964a919e6c7024ba0d4d
CHALCOGEN <166417113+CHALCOGEN645@users.noreply.github.com>
2024-06-10 09:49:03 +0530

    Update TempGPT.py

--- ANNIEMUSIC/plugins/tools/TempGPT.py
+++ ANNIEMUSIC/plugins/tools/TempGPT.py
@@ -7,7 +7,7 @@ from config import BOT_USERNAME
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 
-@app.on_message(filters.command(["enguin"],  prefixes=["p","P"]))
+@app.on_message(filters.command(["enguin","iri],  prefixes=["p","P","s","S"]))
 async def chat_gpt(bot, message):
     try:
         start_time = time.time()
